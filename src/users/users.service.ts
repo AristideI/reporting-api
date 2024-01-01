cfcb08e6f524651b5d4526a4d2e9b677470c6e60
aristidei <a.isingizwe@alustudent.com>
2024-01-01 22:01:04 +0200

    user handlers

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,4 +11,31 @@ export class UsersService {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
   }
+
+  findAll() {
+    return this.repo.find();
+  }
+
+  findOne(id: number) {
+    return this.repo.findOne({ where: { id } });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const currentUser = await this.findOne(id);
+    if (!currentUser) {
+      throw new Error('user not found');
+    }
+    Object.assign(currentUser, attrs);
+    return this.repo.save(currentUser);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) throw new Error('user with that id not found');
+    return this.repo.remove(user);
+  }
 }
